6a0b8a4a94e6685775439683c1ede2b1440d1bf6
Aditya Singh <singhaditya0020@gmail.com>
2025-08-21 15:50:20 +0530

    exploring get,post ,delete

--- src/app.js
+++ src/app.js
@@ -2,11 +2,28 @@ const express=require ("express");
 
 const app=express();
 let name="Aditya";
-app.use((req,res)=>{
-    res.send(`Hi my name is ${name}`)
+
+app.use("/hello", (req, res) => {
+  res.send(`Hello Your name is ${name}`)
 })
 
+app.get("/user",(req,res)=>{
+  res.send({Name:"Aditya",
+           LastName:"Singh",
+           Rollno:"22104B2012",
+           Residence:"Mumbai"
+  }
+  )
+})
+
+app.post("/user",(req,res)=>{
+  res.send("Data Sent Succesfully")
+});
+
+app.delete("/user",(req,res)=>{
+  res.send("Data Deleted Succesfully")
+});
 
  app.listen(3000,()=>{
     console.log("Server is Succesfully Running on Port-3000");
- })
+ });
