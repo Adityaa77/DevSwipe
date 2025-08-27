edc3fac67fb7bfdc2d5d91e8fd0ffb7f0d38797f
Aditya Singh <singhaditya0020@gmail.com>
2025-08-28 00:06:39 +0530

    adding user auth

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -9,6 +9,25 @@ const AdminAuth=(req,res,next)=>{
     }
 };
 
+const jwt=require("jsonwebtoken");
+const user=require("../models/user");
+const UserAuth=async (req,res,next)=>{
+    //reading then toker from the cookies
+    try{const {token}=req.cookies;
+    
+    const decodeObj=await jwt.verify(token,"Dev@Swipe$2004")
+    
+    const{_id}=decodeObj;
+
+    const user=await user.findById(_id);
+    if(!user){
+        throw new Error("User Not Found");
+    }
+    next();}catch(err){
+        res.status(400).send("Error:"+err.message);
+    }
+}
+
 module.exports={
     AdminAuth,
 };
